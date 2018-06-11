127b8d04baea3e1094f3eb4768cb57c141b4534f
hskim <abc@example.com>
2018-06-11 12:49:02 +0900

    add div func2

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -2,5 +2,6 @@ public class Calc
 {
     public int add(int x, int y) { return x+y; }
     public int sub(int x, int y) { return x-y; }
-    public int multi(int x, int y) {return x*y; }
+    public int multi(int x, int y) { return x*y; }
+    public int div(int x, int y) { return x/y; }
 }
